d9837ef1bb7a4920424883953dbb78724f02986c
Yarin Bukai <72891249+byarin90@users.noreply.github.com>
2023-01-25 17:50:45 +0200

    ok

--- src/shared/routes/appRoutes.jsx
+++ src/shared/routes/appRoutes.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from '../layout/client/layout'
+import Home from '../../components/home';
+import Layout from '../layout/client/layout';
 // import Home 
 const AppRoutes = () => {
   return (
     <Router>
-        <Routes>
-            <Route path='/' element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          dasads
-            </Route>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+
+        </Route>
+      </Routes>
     </Router>
   )
 }
