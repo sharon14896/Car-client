86141a9106ed1998e141e360b1a700822317a0b0
Yarin Bukai <72891249+byarin90@users.noreply.github.com>
2023-01-25 17:51:02 +0200

    ok

--- src/shared/routes/appRoutes.jsx
+++ src/shared/routes/appRoutes.jsx
@@ -9,7 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-
+    
         </Route>
       </Routes>
     </Router>
